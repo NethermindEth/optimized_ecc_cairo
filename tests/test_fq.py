e0f008c486bd7e8eda9fe5a094e74dcecddb028f
albert_g <97292142+albert-garreta@users.noreply.github.com>
2022-05-31 00:31:34 +0200

    Fixed test for is_square and created a is_square_specific (short version of is_square)

--- tests/test_fq.py
+++ tests/test_fq.py
@@ -116,13 +116,36 @@ async def test_fq_sub(fq_factory, x, y):
 @settings(deadline=None)
 @pytest.mark.asyncio
 async def test_fq_is_square(fq_factory, x):
+    print(x)
     contract = fq_factory
 
     execution_info = await contract.is_square(split(x)).call()
 
-    result = pack(execution_info.result[0])
-    python_result = pow(x, int((field_modulus - 1) / 2))
+    result = execution_info.result[0]
+    python_result = pow(x, int((field_modulus - 1) / 2), field_modulus)
     python_result = 1 if python_result >= 0 else 0
+    assert result == python_result
+
+
+
+
+@pytest.mark.asyncio
+async def test_fq_is_square_specific(fq_factory):
+    x = 12345    
+    contract = fq_factory
+
+    execution_info = await contract.is_square(split(x)).call()
+
+    result = execution_info.result[0]
+    python_result = pow(x, int((field_modulus - 1) / 2), field_modulus)
+    
+    # This `if` is checking whether `python_result` is -1 modulo `field_modulus``
+    if (python_result - (-1)) % field_modulus == 0:
+        # In this case `x` is not a square
+        python_result = 0
+    else:
+        # Otherwise it is
+        python_result = 1
     assert result == python_result
 
 
