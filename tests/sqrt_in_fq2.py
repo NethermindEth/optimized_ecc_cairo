245b028b68d4662bde0795f3384fc1abc9e48507
Genya Zaytman <yevgeny@nethermind.io>
2022-11-01 15:04:17 -0400

    Fixed the has_squareroot function.

--- tests/sqrt_in_fq2.py
+++ tests/sqrt_in_fq2.py
@@ -6,53 +6,9 @@ from sqrt_mod_p import get_square_root_mod_p as field_sqrt
 FQ2_order = q**2 - 1
 eighth_roots_of_unity = [FQ2([1, 1]) ** ((FQ2_order * k) // 8) for k in range(8)]
 
-
-# Adapted from https://github.com/ethereum/trinity/blob/a1b0f058e7bc8e385c8dac3164c49098967fd5bb/eth2/_utils/bls.py#L63-L77
 def has_squareroot(value):
     a, b = value.coeffs[0].n, value.coeffs[1].n
-
-    if b == 0:
-        leg_symbol = pow(a, (q - 1) // 2, q)
-        if (leg_symbol + 1) % q == 0:
-            return 0
-        return 1
-    l = a**2 + b**2
-    legendre_l = pow(l, (q - 1) // 2, q)
-    if (legendre_l + 1) % q == 0:
-        return 0
-    success, l = field_sqrt(l, q)
-    delta = (a + l) * pow(2, -1, q)
-    legendre_delta = pow(delta, (q - 1) // 2, q)
-    if (legendre_delta + 1) % q == 0:
-        delta = (a - l) * pow(2, -1, q)
-    legendre_delta = pow(delta, (q - 1) // 2, q)
-    if (legendre_delta + 1) % q == 0:
+    leg_symbol = pow(a*a + b*b, (q - 1) // 2, q)
+    if (leg_symbol + 1) % q == 0:
         return 0
     return 1
-
-
-def has_squareroot_v2(value):
-    a, b = value.coeffs[0].n, value.coeffs[1].n
-    if a == 0:
-        if b == 0:
-            return 1
-        else:
-            return 0
-    else:
-        if b == 0:
-            leg_symbol = pow(a, (q - 1) // 2, q)
-            if (leg_symbol + 1) % q == 0:
-                return 0
-            else:
-                return 1
-        else:
-            l = a**2 + b**2
-            success, l_sqrt = field_sqrt(l, q)
-            if success == 0:
-                return 0
-            k = (-a + l_sqrt) * pow(2, -1, q)
-            success, r1 = field_sqrt(k, q)
-            if success == 0:
-                return 0
-            # In this case the sqrt is ( b / (2*r1), r1 )
-            return 1
